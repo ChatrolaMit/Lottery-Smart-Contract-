a79e0ba6c882235aa592ad65e14d662f40f64adc
Chatrola_Mit <chatrolamit047@gmail.com>
2023-01-31 22:06:22 +0530

    initial commit

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,4 +1,21 @@
+from brownie import Lottery , network , config
+from scripts.helpful_scripts import get_account , get_contract
+
 def deploy_lottery(): 
-    pass 
+    account = get_account(id = "alive")
+    lottery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address ,
+        get_contract("link_token").address , 
+        config['networks'][network.show_active()]["fee"],
+        config['networks'][network.show_active()]["keyhash"],
+        {"from" : account} ,
+        publish_souce = config['networks'][network.show_active()].get("verify" , False)      
+    )
+    
+    print("Lottery Deployed")
+    
+    
+ 
 def main(): 
     deploy_lottery()
